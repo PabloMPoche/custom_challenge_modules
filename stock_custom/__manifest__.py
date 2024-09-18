5d84ab77b82fd67c96c4a334ea11f209ccfefd71
PabloMPoche <pabloyoelmercedespoche@gmail.com>
2024-09-18 17:26:06 -0400

    Stock Custom
    Features
    
    - Possibility for search by product sale price from search bar on product template an product product views.

---
+++ stock_custom/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Stock Custom',
+    'version': '1.0',
+    'summary': 'Add customizations on Stock module.',
+    'description': 'Add customizations on Stock module.',
+    'category': 'Stock',
+    'author': 'Pablo Yoel Mercedes Poché',
+    'license': 'LGPL-3',
+    'depends': [
+        'product',
+    ],
+    'data': [
+        'views/product_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
